579ff0651335d68a1d19285fbd623f86cedb0d10
alistair garioch <alistairgarioch@gmail.com>
2020-05-08 21:58:18 +0100

    complete line chart

--- getting_started/chart.js
+++ getting_started/chart.js
@@ -20,16 +20,62 @@ async function drawLineChart() {
   dimensions.boundedHeight =
     dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
-  const wrapper = d3.select("#wrapper");
-  const svg = wrapper.append("svg");
-  svg.attr("width", dimensions.width);
-  svg.attr("height", dimensions.height);
+  const wrapper = d3
+    .select("#wrapper")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
   const bounds = wrapper
     .append("g")
     .style(
       "transform",
       `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
     );
+
+  // yAxis scale
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.boundedHeight, 0]);
+
+  // highlight temperatures below 32F - freezing
+  const freezingTemperaturePlacement = yScale(32);
+  const freezingTemperatures = bounds
+    .append("rect")
+    .attr("x", 0)
+    .attr("width", dimensions.boundedWidth)
+    .attr("y", freezingTemperaturePlacement)
+    .attr("height", dimensions.boundedHeight - freezingTemperaturePlacement)
+    .attr("fill", "#f1f8ff");
+
+  // xAxis scale
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.boundedWidth]);
+
+  // plot line
+  const lineGenerator = d3
+    .line()
+    .x((d) => xScale(xAccessor(d)))
+    .y((d) => yScale(yAccessor(d)));
+  const line = bounds
+    .append("path")
+    .attr("d", lineGenerator(dataset))
+    .attr("fill", "none")
+    .attr("stroke", "#24292e")
+    .attr("stroke-width", 2);
+
+  // plot yAxis
+  const yAxisGenerator = d3.axisLeft().scale(yScale);
+  const yAxis = bounds.append("g").call(yAxisGenerator);
+
+  // plot xAxis
+  const xAxisGenerator = d3.axisBottom().scale(xScale);
+  const xAxis = bounds
+    .append("g")
+    .call(xAxisGenerator)
+    .style("transform", `translateY(${dimensions.boundedHeight}px)`);
 }
 
 drawLineChart();
